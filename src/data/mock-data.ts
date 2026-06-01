f9fccfa1bba663564b457f6d50f982f9a5888c56
MiguelriuDev <126669473+MiguelriuDev@users.noreply.github.com>
2026-06-01 19:15:40 +0000

    Deploying to main from @ Camarai-official/camarai_dash-privado@fb4cc4071d5fb4677948ef3723371d59d9d370af 🚀

--- src/data/mock-data.ts
+++
@@ -1,9 +0,0 @@
-
-export * from './staff';
-export * from './orders';
-export * from './catalog';
-export * from './menu';
-export * from './environments';
-export { initialDevices as mockDevices } from './devices';
-export { initialEstablishments as mockEstablishments } from './establishments';
-export * from './reportes';
--- src/types/reports.ts
+++
@@ -1,7 +0,0 @@
-export type Movement = {
-  id: string;
-  concept: string;
-  type: 'Ingreso' | 'Gasto';
-  paymentMethod: 'Efectivo' | 'Tarjeta' | 'N/A';
-  amount: number;
-};
